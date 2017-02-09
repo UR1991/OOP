40623ebd0bdedf884b7970b8ca22d67186b14a5b
UR1991 <ultimaratio1991@gmail.com>
2017-02-09 07:17:47 +0300

    Add new functions

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php 
+//Create class "Boy"
+Class Boy 
+{	
+	//Print name
+	public function disp ($name)
+	{
+		echo "$name: ";
+	}
+	
+	//function show what the object should to do now
+	public function toDo($nowTime)
+	{
+		if($nowTime[hours] >= 12)
+		{
+			echo "Plays.<br/>";
+		}
+		else 
+		{
+			echo "Sleep.<br/>";
+		}
+	}
+}
+
+//Create class "Girl"
+Class Girl 
+{	
+	//print name
+	public function disp ($name)
+	{
+		echo "$name: ";
+	}
+	// function counts the number of letters in name
+	public function nameStrlen($name)
+	{
+		echo $nameStr = mb_strlen($name)." letters in name.<br/>";
+	}
+}
+
+//Open files with names
+$boysNamesArray = file("Boys.txt");
+$girlsNamesArray = file("Girls.txt");
+
+//Get date
+$time = getdate();
+
+echo "<b>Boys:</b><br/>";
+
+//Create objects and use all functions in class Boy
+foreach ($boysNamesArray as $nameBoy)
+{
+	$boy = new Boy();
+	$boy->disp($nameBoy);
+	$boy->toDo($time);
+}
+
+echo "<b>Girls:</b><br/>";
+
+//Create objects and use all functions in class Girl
+foreach ($girlsNamesArray as $nameGirl)
+{
+	$girl = new Girl();
+	$girl->disp($nameGirl);
+	$girl->nameStrlen(trim($nameGirl));
+}
